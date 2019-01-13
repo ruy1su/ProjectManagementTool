abe54c49e74296b7e94a8da8c9cd5cfdbdc4ca41
Eric <zixia@umail.ucsb.edu>
2019-01-12 22:20:38 -0800

    Create Exceptions for Duplicates Project ID

--- src/main/java/com/zixiaww/ppmtool/services/ProjectService.java
+++ src/main/java/com/zixiaww/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.zixiaww.ppmtool.services;
 
 import com.zixiaww.ppmtool.domain.Project;
+import com.zixiaww.ppmtool.exceptions.ProjectIdException;
 import com.zixiaww.ppmtool.repositories.ProjectRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ public class ProjectService {
 
 
     public Project saveOrUpdateProject(Project project){
-
-        //Logic
-
-        return projectRepositories.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepositories.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
--- src/main/java/com/zixiaww/ppmtool/web/ProjectController.java
+++ src/main/java/com/zixiaww/ppmtool/web/ProjectController.java
@@ -1,17 +1,21 @@
 package com.zixiaww.ppmtool.web;
 
 import com.zixiaww.ppmtool.domain.Project;
+import com.zixiaww.ppmtool.services.MapValidationErrorService;
 import com.zixiaww.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,14 +24,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()){
-            return new ResponseEntity<String>("Invalid project Object", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 }
